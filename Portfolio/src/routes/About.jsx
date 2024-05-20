39e26f247a156b7b1199488221d866c71b7760ef
Jason Pham <deh5yb@virginia.edu>
2024-05-20 15:20:45 -0400

    Add simple about page

--- Portfolio/src/routes/About.jsx
+++ Portfolio/src/routes/About.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography,ListItem } from "@mui/material";
 
 export const About = () => {
   return (
     <Box sx={{ my: "30px" }}>
-        <Typography variant="h3" component="h1">
-          About Page
-        </Typography>
+      <Typography variant="h4" component="h1">
+        About Page
+      </Typography>
+      <Box sx={{ my: "10px" }}>
+      <Typography variant="h5">Home Town</Typography>
+        <ListItem>Norfolk, Virginia</ListItem>
+        <Typography variant="h5">Education</Typography>
+        <ListItem>University of Virginia 2025 BACS</ListItem>
+        <Typography variant="h5">Skills</Typography>
+        <ListItem>Python, Java, JS, C</ListItem>
+      </Box>
     </Box>
   );
 };
-
